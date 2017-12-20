25b5d13c3a34a8a46956b114751d56786a947a41
burak akyol <burakakyol0795@gmail.com>
2017-12-20 22:02:48 +0300

    Added project and project user type

---
+++ src/types/project.js
@@ -0,0 +1,26 @@
+// @flow
+import { type User } from './user';
+import {
+  typeof PROJECT_MANAGER,
+  typeof PROJECT_MEMBER,
+  typeof PROJECT_OWNER,
+} from '../enums/projectRoles';
+import { type Process } from './process';
+
+export type Project = {
+  id: number,
+  title: string,
+  description: string,
+  startDate: string,
+  endDate: string,
+  members: Array<ProjectUser>,
+  processes: Array<Process>,
+};
+
+export type ProjectUser = {
+  id: number,
+  user: User,
+  isActive: boolean,
+  joiningDate: string,
+  role: PROJECT_MANAGER | PROJECT_MEMBER | PROJECT_OWNER,
+};
