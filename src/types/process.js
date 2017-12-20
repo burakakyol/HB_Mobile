5499795e4f9cb4d692292c0eb6a37a2121222915
burak akyol <burakakyol0795@gmail.com>
2017-12-20 22:02:29 +0300

    Added process and process user types

---
+++ src/types/process.js
@@ -0,0 +1,21 @@
+// @flow
+import { type ProjectUser } from './project';
+import { typeof PROCESS_MEMBER, typeof PROCESS_MANAGER } from '../enums/processRoles';
+
+export type Process = {
+  id: number,
+  title: string,
+  description: string,
+  startDate: string,
+  endDate: string,
+  isCompleted: boolean,
+  isActive: boolean,
+};
+
+export type ProcessUser = {
+  id: number,
+  user: ProjectUser,
+  process: Process,
+  isActive: boolean,
+  role: PROCESS_MEMBER | PROCESS_MANAGER,
+};
