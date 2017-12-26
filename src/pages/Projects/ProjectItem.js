9c378a7e8102eeaaf2ff776e39ffabc194684e43
Atölye15 <burakakyol0795@gmail.com>
2017-12-26 16:54:37 +0300

    some changes

--- src/pages/Projects/ProjectItem.js
+++ src/pages/Projects/ProjectItem.js
@@ -1,15 +1,24 @@
+// @flow
 import React, { Component } from 'react';
 import { Container, Content, Card, CardItem, Icon, Text, Right, Button } from 'native-base';
-
+import { type Project } from '../../types/project';
 import { PROJECT_MANAGER, PROJECT_MEMBER, PROJECT_OWNER } from '../../enums/projectRoles';
 
-const ProjectItem = ({ project, onClick }) => (
+type Props = {
+  project: Project,
+  navigation: any,
+  currentProjectAction: Function,
+};
+
+const ProjectItem = ({ project, navigation, currentProjectAction, test }: Props) => (
   <CardItem>
     <Button
       transparent
       onPress={() => {
         console.log('basildi');
-        onClick();
+        navigation.navigate('ProjectPage');
+        currentProjectAction(project);
+        console.log(project);
       }}
     >
       <Icon active name="ios-archive" />
