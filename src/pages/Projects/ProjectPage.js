f7d4fd7a798bd67f863364619cf5aed59c9f1b2f
Atölye15 <burakakyol0795@gmail.com>
2017-12-26 16:22:28 +0300

    Created project page

--- src/pages/Projects/ProjectPage.js
+++ src/pages/Projects/ProjectPage.js
@@ -1,9 +1,19 @@
 // @flow
 
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { View } from 'react-native';
+import { Card, Button, Text } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 type Props = {
   navigation: any,
 };
 
-class ProjectPage extends Component {}
+class ProjectPage extends Component {
+  render() {
+    return <Text>Project Page</Text>;
+  }
+}
+
+export default ProjectPage;
