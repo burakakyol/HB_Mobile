2af7ba75c31d3745501b4cb41255a52720474b73
burak akyol <burakakyol0795@gmail.com>
2017-12-30 23:25:04 +0300

    bug fixed

--- src/pages/Projects/ProjectPage.js
+++ src/pages/Projects/ProjectPage.js
@@ -2,9 +2,9 @@
 
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
-import { View, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { View, ActivityIndicator, TouchableOpacity, Button } from 'react-native';
 import { Card } from 'react-native-elements';
-import { ListItem, Text, Separator, Container, Header, Content, Button, Right } from 'native-base';
+import { ListItem, Text, Separator, Container, Header, Content, Right } from 'native-base';
 import { connect } from 'react-redux';
 import { type Project } from '../../types/project';
 import { type Process } from '../../types/process';
@@ -18,6 +18,7 @@ import {
   clearCurrentProject as clearProject,
 } from '../../redux/modules/project';
 import * as types from '../../enums/actionStatus';
+import * as roleTypes from '../../enums/projectRoles';
 
 type Props = {
   navigation: any,
@@ -28,14 +29,17 @@ type Props = {
   clearProject: Function,
   clearProcesses: Function,
   setCurrent: Function,
+  user: any,
 };
 
 class ProjectPage extends Component<Props, any> {
+ 
   static navigationOptions = ({ navigation }) => ({
     headerRight: <Button title="Kişi Ekle" onPress={() => navigation.navigate('SearchPage')} />,
   });
   constructor(props) {
     super(props);
+    this.checkUserRole = this.checkUserRole.bind(this);
   }
 
   componentWillMount() {
@@ -45,7 +49,13 @@ class ProjectPage extends Component<Props, any> {
       this.props.getMembers(this.props.project.currentProject.id);
     }
   }
+  checkUserRole() {
+    const currentMember = this.props.project.currentProject.members.find(
+      user => user.user.id === this.props.user.user.id,
+    );
 
+    return currentMember.role !== roleTypes.PROJECT_MEMBER;
+  }
   componentWillUnmount() {
     console.log('will unmount');
     this.props.clearProject();
@@ -120,6 +130,7 @@ function mapStateToProps(state) {
   return {
     project: state.project,
     process: state.process,
+    user: state.currentUser,
   };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectPage);
